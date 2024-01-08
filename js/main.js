548b6e4b8ab25e7663bbf6930c514b8da87de27d
Mahmoud-Ahmed-Awad <mahmoud@localhost.localdomain>
2024-01-08 10:38:37 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+let taskInp = document.getElementById("task-inp");
+let addBtn = document.getElementById("add-btn");
+let tasksDiv = document.getElementById("tasks-div");
+
+function showTask(taskId, taskName, done = false) {
+  let taskDiv = document.createElement("div");
+  taskDiv.className = "task card mt-3";
+  if (done == true) {
+    taskDiv.classList.add("done");
+  }
+  let subTaskDiv = document.createElement("div");
+  subTaskDiv.className =
+    "card-body d-flex justify-content-between align-items-center";
+  subTaskDiv.textContent = taskName;
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn btn-danger";
+  deleteBtn.textContent = "Delete";
+  subTaskDiv.appendChild(deleteBtn);
+  taskDiv.appendChild(subTaskDiv);
+  tasksDiv.appendChild(taskDiv);
+  deleteBtn.addEventListener("click", () => {
+    let curruntTasks = JSON.parse(window.localStorage.getItem("tasks"));
+    let tasks = curruntTasks.filter((e) => e.id != taskId);
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    taskDiv.remove();
+  });
+  taskDiv.addEventListener("click", () => {
+    let curruntTasks = JSON.parse(window.localStorage.getItem("tasks"));
+    let tasks = curruntTasks.map((e) => {
+      if (e.id == taskId) {
+        if (e.done == false) {
+          e.done = true;
+        } else {
+          e.done = false;
+        }
+      }
+      return e;
+    });
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    taskDiv.classList.toggle("done");
+  });
+}
+
+if (window.localStorage.getItem("tasks")) {
+  let curruntTasks = JSON.parse(window.localStorage.getItem("tasks"));
+  curruntTasks.forEach((e) => {
+    showTask(e.id, e.name, e.done);
+  });
+}
+
+addBtn.addEventListener("click", () => {
+  let tasks;
+  let task = {
+    id: 0,
+    name: taskInp.value,
+    done: false,
+  };
+  if (window.localStorage.getItem("tasks")) {
+    let curruntTasks = JSON.parse(window.localStorage.getItem("tasks"));
+    task.id =
+      curruntTasks.length > 0
+        ? curruntTasks[curruntTasks.length - 1].id + 1
+        : 0;
+    tasks = [...curruntTasks, task];
+  } else {
+    tasks = [task];
+  }
+  window.localStorage.setItem("tasks", JSON.stringify(tasks));
+  showTask(task.id, task.name);
+  taskInp.value = "";
+});
